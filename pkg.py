6f6c7171c2ffd5d4e27333caddb0b5912a5e5abb
mon <im@monyuan.com>
2023-05-09 17:13:32 +0800

    feat: build.yml

--- pkg.py
+++ pkg.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import sys
 
-print("正在准备")
+print("Preparing")
 root_path = os.getcwd()
 
 
@@ -24,11 +24,11 @@ def local_rm(dirpath):
 
 
 if os.path.exists(os.path.join(root_path, "build")):
-    print("清空build目录")
+    print("clean build")
     local_rm(os.path.join(root_path, "build"))
 
 if os.path.exists(os.path.join(root_path, "dist")):
-    print("清空dist目录")
+    print("clean dist")
     local_rm(os.path.join(root_path, "dist"))
 
 del_mod_list = []
@@ -51,16 +51,16 @@ local = False
 if local is not True:
     pyinstaller_path = "pyinstaller"
 
-print("正在打包...")
+print("pkg...")
 subprocess.Popen("{} --clean {} -n WeDuck".format(pyinstaller_path, pkg_shell), stderr=subprocess.PIPE,
                  stdout=subprocess.PIPE, shell=True).stdout.readline()
 print("{} --clean {} -n WeDuck".format(pyinstaller_path, pkg_shell))
-print("生成打包文件完成")
+print("pkg done!")
 del_path = os.path.join(root_path, "dist", "WeDuck", "PySide2")
 for d in del_mod_list:
-    print("清理打包文件无用的依赖 %s" % d)
+    print("delete module %s" % d)
     os.remove(os.path.join(del_path, d))
 
-print("清理打包文件无用的翻译文件")
+print("delete translate")
 local_rm(os.path.join(del_path, "translations"))
-print("打包完成，目录：" + os.path.join(os.getcwd(), "dist", "WeDuck"))
+print("done dir：" + os.path.join(os.getcwd(), "dist", "WeDuck"))
