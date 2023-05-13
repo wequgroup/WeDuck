ca9da441ac2b98a8809c601be1dd670441e5bcd6
mon <im@monyuan.com>
2023-05-13 19:20:15 +0800

    feat: 修复无法找到配置文件的问题

--- utils/get_root_path.py
+++ utils/get_root_path.py
@@ -5,3 +5,5 @@ root_path = os.path.dirname(__file__).replace("/utils", "").replace("\\utils", "
 home_path = os.path.join(str(pathlib.Path.home()), "WeDuck")
 if os.path.exists(root_path) is not True:
     os.mkdir(root_path)
+if os.path.exists(home_path) is not True:
+    os.mkdir(home_path)
