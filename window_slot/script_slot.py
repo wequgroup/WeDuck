a26ff69388c3232411127b237a652c01bd518618
Mon <im@monyuan.com>
2023-05-14 21:44:49 +0800

    fix: 修复脚本被覆盖问题

--- window_slot/script_slot.py
+++ window_slot/script_slot.py
@@ -8,17 +8,17 @@ from utils.action_record import ActionRecord
 class Script:
     def __init__(self, win):
         self.win = win
-        self.script_name = str(int(time.time()))
 
     def record_script(self):
+        script_name = str(int(time.time()))
         self.win.log("正在录制脚本...")
         self.win.ScriptButton.setText("正在录制脚本...")
-        action = ActionRecord("{}.txt".format(self.script_name))
+        action = ActionRecord("{}.txt".format(script_name))
         action.run()
-        self.win.log("脚本录制成功，名称为：" + self.script_name)
+        self.win.log("脚本录制成功，名称为：" + script_name)
         self.win.ScriptButton.setText("录制新脚本")
         self.win.show()
-        self.win.ScriptNameEdit.setText(self.script_name)
+        self.win.ScriptNameEdit.setText(script_name)
 
     def show_script(self):
         script_path = os.path.join(home_path, "script")
